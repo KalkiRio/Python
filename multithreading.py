8c19d8d5180da07d3841e579a01cc9483243b14b
RIBHU ROY <ribhuroy@outlook.com>
2025-01-24 21:07:25 +0530

    multithreading

---
+++ multithreading.py
@@ -0,0 +1,24 @@
+import threading
+import time
+
+# def task1(task):
+#     time.sleep(4)
+#     print(f"task 1 completed {task}")
+# def task2():
+#     time.sleep(1)
+#     print("task 2 completed")
+
+# # task1()
+# # task2()
+
+# t1=threading.Thread(target=task1,args=("cleaning",))
+# t2=threading.Thread(target=task2)
+# t1.start()
+# t2.start()
+
+# t1.join()
+# t2.join()
+# print('both tasks are done')
+
+
+
