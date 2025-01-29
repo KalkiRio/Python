fce64bb6f416b05ee8a28eb45e9b6fb2ac082d6a
RIBHU ROY <ribhuroy@outlook.com>
2025-01-29 21:38:57 +0530

    dsa

---
+++ algorithms.py
@@ -0,0 +1,12 @@
+"""bubble sort"""
+def bubble_sort(nums):
+    for i in range(len(nums)-1,0,-1):
+        for j in range(i):
+            if nums[j]>nums[j+1]:
+                nums[j],nums[j+1]=nums[j+1],nums[j]
+    return nums
+
+nums=[5,3,8,6,7,2]
+print(bubble_sort(nums))
+
+"""selection sort"""
