7f31673119460c9f5cbe62e9d1eb0fa3c16334a8
RIBHU ROY <ribhuroy@outlook.com>
2025-02-17 09:36:36 +0530

    pattern

--- leetcode.py
+++ leetcode.py
@@ -151,48 +151,7 @@
 
 
 
-#                         *    
-#                         * *  
-#     *       * * * e e e * * *
-#   * *       * * *       * *  
-# * * * e e e * * *       *    
-#   * *                        
-#     *                        
-# n=int(input("enter a number: "))
-# for i in range(n-1):
-#     for j in range(n*4):
-#         print(' ',end=' ')
-#     for j in range(i+1):
-#         print('*', end=' ')
-#     print()
-# for i in range(n):
-#     for j in range(i,n-1):
-#         print(' ',end=' ')
-#     for j in range(i+1):
-#         print('*', end= ' ')
-#     for j in range(n):
-#         if i==n-1:
-#             print('e',end=' ')
-#         else:
-#             print(' ',end=' ')
-#     for j in range(n):
-#         print('*',end=' ')
-#     for j in range(n):
-#         if i==0:
-#             print('e', end=' ')
-#         else:
-#             print(' ',end=' ')
-#     for j in range(i,n):
-#         print('*',end=' ')
-#     print()
-# for i in range(n-1):
-#     for j in range(i+1):
-#         print(' ', end=' ')
-#     for j in range(i,n-1):
-#         print('*',end=' ')
-#     for j in range(n):
-#         print(' ',end=' ')
-#     print()
+
 
 # n=5
 # for i in range(n):
