60da91eec6f151a590c6807c6b57a1192447b555
RIBHU ROY <ribhuroy@outlook.com>
2025-02-18 19:27:08 +0530

    file handling

--- patterns.py
+++ patterns.py
@@ -111,3 +111,76 @@
 #         else:
 #             print(" ",end=" ")
 #     print()
+
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         if i in (1,5) or j in (1,5):
+#             print("*",end=' ')
+#         else:
+#             print(' ',end=' ')
+#     print()
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         if i ==1 or j==1:
+#             print("*",end=' ')
+#         else:
+#             print(' ',end=' ')
+#     print()
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         if i ==5 or j==1:
+#             print("*",end=' ')
+#         else:
+#             print(' ',end=' ')
+#     print()
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         if i ==1 or j==5:
+#             print("*",end=' ')
+#         else:
+#             print(' ',end=' ')
+#     print()
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         if i ==5 or j==5:
+#             print("*",end=' ')
+#         else:
+#             print(' ',end=' ')
+#     print()
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         if j<=i:
+#             print('*',end=' ')
+#         else:
+#             print(' ',end=' ')
+#     print()
+
+# for i in range(1,6):
+#     for j in range(1,6):
+#         if i<=j:
+#             print('*',end=' ')
+#         else:
+#             print(' ',end=' ')
+#     print()
+
+# for i in range(1,6):
+#     for j in range(5,0,-1):
+#         if j<=i:
+#             print("*", end=' ')
+#         else:
+#             print(' ', end=' ')
+#     print()
+
+# for i in range(1,6):
+#     for j in range(5,0,-1):
+#         if i<=j:
+#             print('*',end=' ')
+#         else:
+#             print(' ',end=' ')
+#     print()
