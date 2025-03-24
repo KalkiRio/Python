e6e5175c2fa084ab66593d04c6287c3c17f08334
RIBHU ROY <ribhuroy@outlook.com>
2025-03-24 21:03:06 +0530

    sort

--- patterns.py
+++ patterns.py
@@ -256,26 +256,27 @@
 #       3 2 1 2 3
 #         2 1 2
 #           1 
-n=5
-for i in range(n-1):
-    num=i+1
-    for j in range(i,n):
-        print(" ",end=" ")
-    for j in range(i+1):
-        print(num, end=" ")
-        num-=1
-    for j in range(i):
-        print(num+2, end=" ")
-        num+=1
-    print()
-num+=2
-for i in range(n):
-    for j in range(i+1):
-        print(" ", end=" ")
-    for j in range(i,n):
-        print(num,end=" ")
-        num-=1
-    for j in range(i,n-1):
-        print(num+2,end=" ")
-        num+=1
-    print()
+# n=5
+# for i in range(n-1):
+#     num=i+1
+#     for j in range(i,n):
+#         print(" ",end=" ")
+#     for j in range(i+1):
+#         print(num, end=" ")
+#         num-=1
+#     for j in range(i):
+#         print(num+2, end=" ")
+#         num+=1
+#     print()
+# num+=2
+# for i in range(n):
+#     for j in range(i+1):
+#         print(" ", end=" ")
+#     for j in range(i,n):
+#         print(num,end=" ")
+#         num-=1
+#     for j in range(i,n-1):
+#         print(num+2,end=" ")
+#         num+=1
+#     print()
+
--- rough.py
+++ rough.py
@@ -5,3 +5,13 @@
 #print(t.index()) --> type error
 #print(t.index(100)) --> value error as 100 is not present in the tuple
 
+# l=[2,5,7,9,2,8]
+# l2=sorted(l)
+# for i in range(3):
+#     print(l2[i],end='')
+
+# l=[3,7,1,5,8,5]
+# l2=sorted(l)
+# for i in range(3):
+#     print(l2[i],end='')
+
